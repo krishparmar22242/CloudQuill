08d412fa2c8e3e94a4967d2747a89261f6f0ad5e
krishparmar22242 <your-email@example.com>
2026-03-17 18:42:03 +0530

    Update Navbar to test CI/CD automation hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = (props) => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">CloudQuill</Link>
+                    <Link className="navbar-brand" to="/">CloudQuill (Automated CI/CD)</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
